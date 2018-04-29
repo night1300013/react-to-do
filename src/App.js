51b14abc95a493b25408f381c4af653825ac3e98
ShangMing Cherng <night1300013@gmail.com>
2018-04-28 23:40:08 -0700

    add delete todo

--- src/App.js
+++ src/App.js
@@ -33,12 +33,17 @@ class App extends Component {
     this.setState({ todos: todos });
   }
 
+  deleteToDo(index) {
+    const todos = this.state.todos.filter((todo, current_index) => current_index !== index);
+    this.setState({ todos: todos });
+  }
+
   render() {
     return (
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) =>
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteToDo={ () => this.deleteToDo(index) } />
           )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
